0bfae71e99535dbf3909434fe70ced97308d48e2
Spitfire-Ice <37056025+Spitfire-Ice@users.noreply.github.com>
2019-03-20 00:10:16 +0200

    200319

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,2 @@
 import '../scss/main.scss';
-import './components/common';
+
--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
             options: {
                 outputPath: 'img/',
                 name: '[name].[ext]',
-                publicPath: './img/',
+                publicPath: '../img/',
                 useRelativePaths: true
             },
           },
@@ -86,6 +86,8 @@ module.exports = {
             options: {
               outputPath: 'fonts/',
               name: '[name].[ext]',
+                publicPath: '../fonts/',
+                useRelativePaths: true
             },
           },
         ],
